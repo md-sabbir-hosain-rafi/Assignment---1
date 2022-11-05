e41080ecfac3dcffd4563d6817b6cc00636fdec6
MD Sabbir Hosain Rafi <mdsabbirhosainrafi99@yahoo.com>
2022-11-05 20:16:00 +0600

    first commit

---
+++ cosmetics.js
@@ -0,0 +1,27 @@
+let perfumeList=[
+  {name: 'Royal Mirage', price: 900},
+  {name: 'Dark Fever', price: 500},
+  {name: 'Good Girl', price: 1900},
+  {name: 'Brut', price: 3900},
+  {name: 'Every One', price: 990},
+  {name: 'Axe', price: 400},
+  {name: 'Double Blue', price: 200},
+  {name: 'Active Plus', price: 800},
+  {name: 'King For Man', price: 1200},
+  {name: 'Jaguar Red', price: 1900},
+  {name: 'Jaguar Green', price: 2900},
+  {name: 'Color Me', price: 480},
+  {name: 'Jovan', price: 980},
+  {name: 'Yatch Man', price: 590},
+  {name: 'Bogart', price: 900},
+  {name: 'Royal Spry', price: 480},
+  {name: 'Royal Mini', price: 280},
+  {name: 'Lafz Spry', price: 180},
+  {name: 'Blue Lady', price: 580},
+  {name: 'Blue Man', price: 490},
+  {name: 'Men Spry', price: 280},
+  {name: 'Love Spry', price: 280},
+  {name: 'Gold Spry', price: 280},
+]
+
+export {perfumeList};
---
+++ import.js
@@ -0,0 +1,6 @@
+import {perfumeList} from "./cosmetics.js";
+
+perfumeList.map((item,i)=>{
+    let Details=`Our products name is ${item.name}`
+    console.log(Details)
+})
